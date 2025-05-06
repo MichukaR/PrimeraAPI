21b279140c3d8bc18883279854cf19b70f12d487
Michu <michelle.reyes.delgado@udla.edu.ec>
2025-05-06 09:01:17 -0500

    First Commit

---
+++ Modelos/Bicicleta.cs
@@ -0,0 +1,12 @@
+namespace MiPrimeraAPIMichu.Modelos;
+
+public class Bicicleta
+{
+    
+    public int id{get;set;}
+    public string marca{get;set;}
+    public string material{get;set;}
+    public double valor{get;set;}
+    public bool EsNueva{get;set;}
+    
+}
---
+++ Repositories/BicicletasRepository.cs
@@ -0,0 +1,38 @@
+using MiPrimeraAPIMichu.Modelos;
+
+namespace MiPrimeraAPIMichu.Repositories;
+
+public class BicicletasRepository
+{
+    public IEnumerable<Bicicleta> ObtenerListadoBicicletas()
+    {
+        
+        List<Bicicleta> bicicletas = new List<Bicicleta>();
+        Bicicleta Bicicleta1 = new Bicicleta
+        {
+            id = 1,
+            marca = "Marca1",
+            valor = 1000,
+            material = "Aluminio",
+            EsNueva = true
+        };
+        Bicicleta Bicicleta2 = new Bicicleta
+        {
+            id = 2,
+            marca = "Marca2",
+            valor = 100,
+            material = "Papel",
+            EsNueva = false
+        };
+        bicicletas.Add(Bicicleta1);
+        bicicletas.Add(Bicicleta2);
+        
+        return bicicletas;
+    }
+    
+    public Bicicleta ObtenerInformacionBicicleta(int id)
+    {
+        var bicicleta = ObtenerListadoBicicletas().First(x => x.id == id);
+        return bicicleta;
+    }
+}
